762993291fef729b1774f8a030430c43f8666842
Angel Mae Lucero <angelmae820@gmail.com>
2021-12-11 12:56:42 +0800

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'swertekapagnahulaanmo'
+
+#index
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#about
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+#login
+@app.route("/login", methods=["POST","GET"])
+def login():
+    return render_template("login")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h>"
+
+#prediction
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
