2659b9c8380757c9356ac5baa6fdda92ac9933b1
LuceroAngelMae <bg201802170@wmsu.edu.ph>
2021-12-12 22:27:05 +0800

    init

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask 
+from flask import Flask, render_template
+from flask.templating import render_template_string
 from flask_sqlalchemy import SQLAlchemy 
-from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter 
+from flask_user import login_required, UserManager, UserMixin
 from flask_mail import Mail 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['SECRET_KEY'] = 'swertemopagnahulaanmo'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['CSRF_ENABLED'] = True 
 app.config['USER_ENABLE_EMAIL'] = True 
-app.config['USER_APP_NAME'] = 'AgriVance Pro'
+app.config['USER_APP_NAME'] = 'AgriVance_Pro'
 app.config['USER_AFTER_REGISTER_ENDPOINT'] = 'user.login'
 app.config.from_pyfile('config.cfg')
 
@@ -22,14 +23,14 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
     email = db.Column(db.String(255), nullable=False, unique=True)
-    confirmed_at = db.Column(db.DateTime())
+    email_confirmed_at = db.Column('confirmed_at', db.DateTime())
 
-db_adapter = SQLAlchemyAdapter(db, User)
-user_manager = UserManager(db_adapter, app)
+
+user_manager = UserManager(app, db, User)
 
 @app.route('/')
 def index():
-    return '<h1>This is the home page!</h1>'
+    return render_template('index.html')
 
 @app.route('/profile')
 @login_required
