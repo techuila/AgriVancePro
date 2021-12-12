f82f08968c89505591ae89eeddc10db467d2b680
LuceroAngelMae <bg201802170@wmsu.edu.ph>
2021-12-12 12:57:44 +0800

    init

--- app.py
+++ app.py
@@ -1,59 +1,40 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter 
+from flask_mail import Mail 
 
-app=Flask(__name__)
-db= SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLACHEMY_DATABASE_URI']= 'sqlite:///database.db'
-app.config['SECRET_KEY']= 'swertekapagnahulaanmo'
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisisasecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['CSRF_ENABLED'] = True 
+app.config['USER_ENABLE_EMAIL'] = True 
+app.config['USER_APP_NAME'] = 'AgriVance Pro'
+app.config['USER_AFTER_REGISTER_ENDPOINT'] = 'user.login'
+app.config.from_pyfile('config.cfg')
+
+db = SQLAlchemy(app)
+mail = Mail(app)
 
 class User(db.Model, UserMixin):
-      id = db.Column(db.Integer, primary_key=True)
-      username = db.Column(db.String(20), primary_key=True, unique=True)
-      password = db.Column(db.String(80), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    active = db.Column(db.Boolean(), nullable=False, server_default='0')
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
 
-class RegisterForm(FlaskForm):
-       username= StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-       password= StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Password"})
-       submit =SubmitField ("Register")
-
-       def validate_username(self,username):
-             existing_user_username = User.query.filter_by(username=username.data).first()
-
-             if existing_user_username:
-                   raise ValidationError("Username Already exists! Please Try Again.")
-
-class LoginForm(FlaskForm):
-       username= StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-       password= StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Password"})
-       submit =SubmitField ("Login")
+db_adapter = SQLAlchemyAdapter(db, User)
+user_manager = UserManager(db_adapter, app)
 
 @app.route('/')
-def home():
-  return render_template('home.html')
+def index():
+    return '<h1>This is the home page!</h1>'
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-      form=LoginForm()
-      return render_template('login.html' , form=form)
-
-@app.route('/register',methods=['GET','POST'])
-def register():
-      form=RegisterForm()
-
-      if form.validate_on_submit():
-            hashed_password = Bcrypt.generate_password_hashed(form.password.data)
-            new_user=User(username=form.username.data, password=hashed_password)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('login'))
-
-      return render_template('register.html', form=form)
+@app.route('/profile')
+@login_required
+def profile():
+    return '<h1>This is the protected profile page!</h1>'
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True) 
