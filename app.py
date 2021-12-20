ec9662ac83f7daddb5bf664ceaf0d8a5147c46d7
LuceroAngelMae <bg201802170@wmsu.edu.ph>
2021-12-20 10:22:28 +0800

    ini

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Yield Predicted {} kg/ha'.format(output))
+    return render_template('prediction.html', prediction_text='Yield Predicted {} kg/ha'.format(output))
 
 @app.route('/results',methods=['POST'])
 def results():
